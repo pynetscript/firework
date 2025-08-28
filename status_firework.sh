85c0891296b0071e836192604c1106764b7c064e
Aleks Lambreca <31127654+pynetscript@users.noreply.github.com>
2025-08-28 17:56:17 +0300

    Update status_firework.sh

--- status_firework.sh
+++ status_firework.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Ensure you are in your project's root directory (e.g., ~/firework) before running this script ---
+# This script checks the Firework application services (Gunicorn via systemd, and Nginx).
+
 sudo systemctl status firework | grep active
 sudo systemctl status nginx | grep active
